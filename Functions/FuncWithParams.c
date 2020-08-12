856b832ae9585b5bb11030c5c2ffe58fd3b29d96
Omar <omar.belkady@g.austincc.edu>
2020-08-12 20:04:11 +0100

    How To Create A Function That Takes Arguments

--- Functions/FuncWithParams.c
+++ Functions/FuncWithParams.c
@@ -3,9 +3,29 @@
 
 void funcWithParams(char name[], char favSinger[]);
 
+int biggerNum(int firstNum, int secondNum)
+{
+	int output;
+	if(firstNum > secondNum)
+	{
+		printf("The first Number: %d is bigger than %d\n",firstNum,secondNum);
+		output = firstNum;
+		return output;
+	}
+
+	else
+	{
+		printf("The second Number: %d is bigger than %d \n",secondNum,firstNum);
+		output=secondNum;
+		return output;
+	}
+}
+
 int main()
 {
 	funcWithParams("C And Assembly","Taylor Swift");
+	printf("%d",biggerNum(30,65));
+	printf("\n");
 	return 0;
 }
 
