42b4c23d76f0bc56c78e6e99ba3ef94bf8a66fe7
Omar <omar.belkady@g.austincc.edu>
2020-08-12 01:41:58 +0100

    Some Basic Programs for 6342_56837 TO C BECAUSE HE LOVES C!

---
+++ Simple_Progs/Variables/Prog1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char name[] = "Nelan's fav Prog Lang is: ";
+	char favProgLan[] = "C ON TOP OF MAchine Language Because He Loves Neha";
+	strcat(name,favProgLan);
+	printf("When I concatenated the string %s\n",name);
+	return 0;
+}
