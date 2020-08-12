97379c8b2d2a182ec5835482aec0d73ecd3848ef
Omar <omar.belkady@g.austincc.edu>
2020-08-12 12:18:13 +0100

    New Progs

---
+++ String_Formating.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("Nelan's is a %s and his favorite number is %d","C-Lover", 2526);
+	//%s = string
+	//%d = integer
+	//%f = float/double
+	
+	int favNumberAndIndiv = 2526;
+	printf("\n His favorite number big time is %d!!!",favNumberAndIndiv);
+
+	return 0;
+}
