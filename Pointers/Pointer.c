f0e0d88705d93987ec9be691dfba9882f5efdb45
Omar <omar.belkady@g.austincc.edu>
2020-08-13 16:14:38 +0100

    How To Use Pointers in C

---
+++ Pointers/Pointer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//A pointer is a type of data in C that is a memory address
+
+	int fav_Number_for_2526=6342;
+
+	printf("The address in memory of fav_Number is: %p",&fav_Number_for_2526);
+
+	printf("\n\t\tExample2: \n");
+	//I can create a pointer within a pointer variable similar to a double,int, char
+	//But I have to store the memory address of a variable that is already defined within my program
+	int *alan_fav_Subj = &fav_Number_for_2526;
+
+	double funnyNumber = 63526.263829568;
+	double *ptr_funnyNumber = &funnyNumber;
+	printf("Address: %p",&ptr_funnyNumber);
+	printf("\n");
+	printf("Address: %p",&alan_fav_Subj);
+
+
+	//<==============HOW TO DEREFERENCE A POINTER====================>
+	printf("\n\n\nHOW TO DEREFENCE: \n");
+	printf("I will now dereference *ptr_funnyNumber var: %f",*ptr_funnyNumber);
+	return 0;
+}
